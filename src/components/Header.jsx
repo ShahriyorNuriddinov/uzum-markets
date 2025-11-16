7ec2e889062a5ec9d43bfde8cde66c1de1726347
ShahriyorNuriddinov <nuriddinovlar20@gmail.com>
2025-11-16 18:20:59 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="">
+        <div className="container1">salom dunyo</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
